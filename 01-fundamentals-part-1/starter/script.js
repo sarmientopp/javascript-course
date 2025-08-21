3cfa26da3fa078c8709a7f67de0ee6c13ec295b1
Princess Jolyn Sarmiento <sarmientojlyn1218@gmail.com>
2025-08-21 17:01:38 +0800

    feat: add basic variables and data types

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,64 @@
-// JavaScript Fundamentals - Part 1
-// We'll write our code here!
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("=== VARIABLES ===");
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let age = 30;
+age = 31;
+console.log(age);
+
+const birthYear = 1991;
+console.log(birthYear);
+
+const PI = 3.1415;
+console.log(PI);
+
+var job = "programmer";
+job = "teacher";
+console.log(job);
+
+const country = "Portugal";
+const language = "Portuguese"; 
+const population = 10;
+console.log(country, language, population);
+
+age = 26;
+firstName = "Sarah";
+let myCurrentJob = "teacher";
+
+console.log(firstName, age, myCurrentJob, PI);
+
+console.log("=== DATA TYPES ===");
+
+let newAge = 23; 
+console.log(newAge);
+console.log(typeof newAge);
+
+let anotherName = "Princess"; 
+console.log(anotherName);
+console.log(typeof anotherName);
+
+let javascriptIsFun = true; 
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+let dynamicVariable = 23;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+dynamicVariable = "Now I'm a string!";
+console.log(dynamicVariable, typeof dynamicVariable);
+
+dynamicVariable = true;
+console.log(dynamicVariable, typeof dynamicVariable);
+
+console.log(typeof 42);
+console.log(typeof "Hello");
+console.log(typeof true);
+console.log(typeof undefined);
